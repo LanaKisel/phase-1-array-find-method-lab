3d8d29138257fc29fce6437441106ad6cf281590
Lana Kisel <svetlana.kisel.ca@gmail.com>
2023-07-25 16:02:49 -0700

    find method in an array

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// code your solution here
+const record = [
+    {
+        year: '2020',
+        result: 'L'
+    },
+    {
+        year: '2021',
+        result: 'W'
+    },
+    {
+        year: '2022',
+        result: 'N/A'
+    }
+]
+
+
+
+function superbowlWin(superbowlRecord) {
+    const found = superbowlRecord.find(({result}) => {
+     return  result === "W"
+    });
+     if (found === undefined) {
+       return undefined
+     } else {
+       return found.year 
+     }
+    }
+  console.log(superbowlWin(record))
+  
